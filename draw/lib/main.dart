17bb28027246bbf24ac7189a205f9eac13eb75cd
Aquarian-Age <36611556+Aquarian-Age@users.noreply.github.com>
2022-10-12 13:04:41 +0800

    draw 坐标跟随圆周

--- draw/lib/main.dart
+++ draw/lib/main.dart
@@ -1,10 +1,8 @@
-import 'dart:io';
 import 'dart:math';
 
-import 'package:draw/dr/dr.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:window_size/window_size.dart';
+
+import 'dr/dr.dart';
 
 class MyPainter extends CustomPainter {
   @override
@@ -25,26 +23,7 @@ class MyPainter extends CustomPainter {
   bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
 }
 
-const double windowWidth = 728;
-const double windowHeight = 728;
-void setupWindow() {
-  if (!kIsWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
-    WidgetsFlutterBinding.ensureInitialized(); //调用 [runApp] 之前初始化绑定
-    setWindowMinSize(const Size(windowWidth, windowHeight));
-    setWindowMaxSize(const Size(windowWidth, windowHeight));
-    getCurrentScreen().then((screen) {
-      //启动居中?
-      setWindowFrame(Rect.fromCenter(
-        center: screen!.frame.center,
-        width: windowWidth,
-        height: windowHeight,
-      ));
-    });
-  }
-}
-
 void main() {
-  setupWindow();
   runApp(const MyApp());
 }
 
@@ -69,17 +48,16 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int left = 0, top = 0;
   double degree = 210.0;
-
   void getxy(double width, double height) {
     var r = min(width / 2, height / 2);
     print("r==$r");
 
     DR dr = DR();
     double radians = dr.degreeToRadian(degree);
-    double x1 = r + r * cos(radians);
-    double y1 = r + r * sin(radians);
+    double x1 = width / 2 + r * cos(radians);
+    double y1 = height / 2 + r * sin(radians);
 
-    print("$x1 , $y1");
+    // print("$x1 , $y1");
     print("x1: ${x1.toInt()}, y1: ${y1.toInt()}");
     setState(() {
       degree;
@@ -93,27 +71,32 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    print(size); //宽度 高度
+    print("屏幕宽/高: $size"); //宽度 高度
 
     getxy(size.width, size.height);
     return Stack(
       children: [
-        CustomPaint(
-          painter: MyPainter(),
-          child: const Center(
-            child: Text(
-              "圆心",
-              style: TextStyle(fontSize: 12, color: Colors.red),
+        SizedBox(
+          width: size.width,
+          height: size.height,
+          child: CustomPaint(
+            painter: MyPainter(),
+            child: const Center(
+              child: Text(
+                "圆心",
+                style: TextStyle(fontSize: 12, color: Colors.red),
+              ),
             ),
           ),
         ),
         Positioned(
-            left: left.toDouble(),
-            top: top.toDouble(),
-            child: Text(
-              "$degree",
-              style: const TextStyle(fontSize: 12, color: Colors.blue),
-            ))
+          left: left.toDouble(),
+          top: top.toDouble(),
+          child: Text(
+            "$degree",
+            style: const TextStyle(fontSize: 12, color: Colors.blue),
+          ),
+        ),
       ],
     );
   }
